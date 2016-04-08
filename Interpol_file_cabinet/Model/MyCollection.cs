e087f8f961c183f2c404fa84a24b4beb7cb2e58e
Dima.Nikitin <nikitin2705@mail.ru>
2016-04-08 22:11:51 +0300

    Класс коллекции всех преступников и группировок

---
+++ Interpol_file_cabinet/Model/MyCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpol_file_cabinet.Model
+{
+    class MyCollection
+    {
+        public static List<Criminal> criminals;
+        public static List<Criminal> criminalsArchive;
+        public static List<Criminal> criminalsDead;
+        public static List<Group> groups;
+
+        // Статический конструктор
+        static MyCollection()
+        {
+            criminals = new List<Criminal>();
+            criminalsArchive = new List<Criminal>();
+            criminalsDead = new List<Criminal>();
+            groups = new List<Group>();
+        }
+    }
+}
