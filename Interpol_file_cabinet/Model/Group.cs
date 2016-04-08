e16860cff7135ce9bc211956d8ac0a897bcaf04e
Dima.Nikitin <nikitin2705@mail.ru>
2016-04-08 22:10:49 +0300

    Класс группировки

---
+++ Interpol_file_cabinet/Model/Group.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpol_file_cabinet.Model
+{
+    public class Group
+    {
+        // Свойства
+        public string Name { get; set; }
+        public List<Criminal> criminalsInGroup = new List<Criminal>();
+
+        // Конструктор с параметрами
+        public Group(string name, List<Criminal> crims)
+        {
+            Name = name;
+            criminalsInGroup = crims;
+        }
+
+        // Конструктор копирования
+        public Group(Group gr)
+        {
+            this.Name = gr.Name;
+            this.criminalsInGroup = new List<Criminal>(gr.criminalsInGroup);
+        }
+
+        // Конструктор по умолчанию
+        public Group() { }
+    }
+}
